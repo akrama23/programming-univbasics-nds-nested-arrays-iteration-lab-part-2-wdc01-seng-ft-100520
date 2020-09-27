4dd91932f8f321c75ce9aff503674ab6b0a578c5
akam alam <arundelfootball23@gmail.com>
2020-09-27 01:36:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ while row_index < src.count do
   while element_index < src[row_index].count do 
     if src[row_index][element_index] < smallest_num
       smallest_num = src[row_index][element_index]
-      return_data << smallest_num
+      
     end 
     element_index += 1 
   end 
